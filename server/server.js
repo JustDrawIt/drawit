800b6eca67ce52384a49b6de354d6c301b42f24f
Cain <cainwatson@users.noreply.github.com>
2018-05-31 13:04:44 -0500

    (scaffold) serve public directory

--- server/server.js
+++ server/server.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const path = require('path');
 
 const server = express();
+const PUBLIC = path.resolve(__dirname, '../public');
+
+server.use(express.static(PUBLIC));
 
 module.exports = server;
