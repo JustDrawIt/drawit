3f199de17852127ef692d4d2cd1d8daa3bb31024
Cain <cainwatson@users.noreply.github.com>
2018-05-31 14:11:28 -0500

    (scaffold) add mocha and chai for server testing

---
+++ server/test/server.spec.js
@@ -0,0 +1,11 @@
+const { expect } = require('chai');
+const request = require('request');
+
+describe('server', () => {
+  it('should serve static files', (done) => {
+    request('http://localhost:8080', (error, response, body) => {
+      expect(body).to.include('</html>')
+      done();
+    });
+  });
+});
