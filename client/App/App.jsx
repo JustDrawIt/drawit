fd88245201303a7d302f915d71fc5a1c66df1ed9
Cain <cainwatson@users.noreply.github.com>
2018-06-01 10:50:42 -0500

    (add) redux dev tools and move create store to /store

--- client/App/App.jsx
+++ client/App/App.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
-import { createStore } from 'redux';
 
-import reducers from '../store/reducers';
+import store from '../store';
 import ScreeenLogin from '../screens/ScreenLogin';
 import ScreenCreateGame from '../screens/ScreenCreateGame';
 import ScreenGame from '../screens/ScreenGame';
-
-const store = createStore(reducers);
 
 const App = () => (
   <Provider store={store}>
