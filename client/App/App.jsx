a4aa9ad6fc9f37b38651b4a3f91081bf1ac55974
Cain <cainwatson@users.noreply.github.com>
2018-06-01 10:32:03 -0500

    (install) react redux, resolves #20

--- client/App/App.jsx
+++ client/App/App.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { createStore } from 'redux';
+
+import reducers from '../store/reducers';
 import ScreeenLogin from '../screens/ScreenLogin';
 import ScreenCreateGame from '../screens/ScreenCreateGame';
 import ScreenGame from '../screens/ScreenGame';
 
+const store = createStore(reducers);
+
 const App = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route path="/login" component={ScreeenLogin} />
-      <Route path="/create_game" component={ScreenCreateGame} />
-      <Route path="/game/:id" component={ScreenGame} />
-      <Redirect to="/login" />
-    </Switch>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/login" component={ScreeenLogin} />
+        <Route path="/create_game" component={ScreenCreateGame} />
+        <Route path="/games/:joinCode" component={ScreenGame} />
+        <Redirect to="/login" />
+      </Switch>
+    </BrowserRouter>
+  </Provider>
 );
 
 export default App;
---
+++ client/store/reducers/create-game.reducer.js
@@ -0,0 +1,12 @@
+import CreateGameActions from '../actions/create-game.actions';
+
+const createGameDefaultState = {
+
+};
+
+export default function createGameReducer(state = createGameDefaultState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
