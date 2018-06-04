1af0548b8a7ce87f05463f778329d5da8510760b
Paul Marinaro <paulmarinaro@gmail.com>
2018-06-04 10:29:54 -0500

    (add) schema and db connection

--- server/config.js
+++ server/config.js
@@ -3,4 +3,5 @@ const path = require('path');
 module.exports = {
   PORT: process.env.PORT || 8080,
   PATH: path.resolve(__dirname, '../public'),
+  MONGODB: process.env.MONGODB,
 };
---
+++ server/database/database.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const { MONGODB } = require('../config');
+
+mongoose.connect(MONGODB);
+
+const db = mongoose.connection;
+db.on('error', () => console.error('connection error'));
+db.once('open', () => console.log('connected to db'));
+
+const gameSchema = mongoose.Schema({
+  joinCode: Number,
+  players: String,
+});
+
+const Game = mongoose.model('Game', gameSchema);
+
+module.exports.Game = Game;
+
