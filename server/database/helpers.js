4b1a557155cf9aaaf8c721d713d94079213a5f46
Jelani <jhankins02@gmail.com>
2018-06-04 18:55:47 -0500

    (create) /codes router , db helper findJoinCode

--- server/database/helpers.js
+++ server/database/helpers.js
@@ -1,1 +1,5 @@
 const db = require('./database');
+
+const findJoinCode = joinCode => db.Game.findOne({ joinCode });
+
+exports.findJoinCode = findJoinCode;
--- server/routers/index.js
+++ server/routers/index.js
@@ -1,5 +1,7 @@
 const gamesRouter = require('./games');
+const codesRouter = require('./codes');
 
 module.exports = (app) => {
   app.use('/games', gamesRouter);
+  app.use('/codes', codesRouter);
 };
