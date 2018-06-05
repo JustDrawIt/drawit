c346a348ad827836e61bc3a03ecf99c6e9e8a7b0
Jelani <jhankins02@gmail.com>
2018-06-04 19:54:20 -0500

    (create) NicknameForm component, add to ScreenGame

---
+++ client/components/NicknameForm/NicknameForm.jsx
@@ -0,0 +1,55 @@
+import React, { PureComponent } from 'react';
+import styled from 'react-emotion';
+import Button from '../Util/Button';
+import Input from '../Util/Input';
+import socket from '../../sockets';
+
+const Container = styled('div')`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+`;
+
+class NicknameForm extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nickname: '',
+      error: '',
+    };
+
+    this.setNickname = this.setNickname.bind(this);
+    this.joinGame = this.joinGame.bind(this);
+  }
+
+  setNickname({ target }) {
+    this.setState({ nickname: target.value, error: '' });
+  }
+
+  joinGame() {
+    const { nickname } = this.state;
+    if (!nickname) {
+      this.setState({ error: 'Please enter a nickname!' });
+    } else {
+      if (!this.props.game.players.includes(nickname)) {
+        socket.emit('join', { nickname });
+      } else {
+        this.setState({ error: 'Nickname already exists!' });
+      }
+    }
+  }
+
+  render() {
+    return (
+      <Container>
+        <h2><b>Enter A Nickname</b></h2>
+        <Input onChange={this.setNickname} placeholder="Nickname" type="text" />
+        <Button onClick={this.joinGame}>Join!</Button>
+        {this.state.error ? <p>{this.state.error}</p> : null}
+      </Container>
+    );
+  }
+}
+
+
+export default NicknameForm;
--- client/screens/ScreenGame.jsx
+++ client/screens/ScreenGame.jsx
@@ -1,5 +1,21 @@
 import React from 'react';
+import styled from 'react-emotion';
+import NicknameForm from '../components/NicknameForm/NicknameForm';
 
-const ScreenGame = () => null;
+const Container = styled('div')`
+  margin: 0;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+const game = { players: ['jelani'] };
+const ScreenGame = () => (
+  <div>
+    <Container>
+      <NicknameForm game={game} />
+    </Container>
+  </div>
+);
 
 export default ScreenGame;
