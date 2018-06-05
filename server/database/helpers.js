c23436ec55d692a14976dc928f56f2de99e2e6b5
Cain <cainwatson@users.noreply.github.com>
2018-06-04 21:58:51 -0500

    (create) add player to game db helper

--- server/database/helpers.js
+++ server/database/helpers.js
@@ -1,5 +1,15 @@
-const db = require('./database');
+const { Game } = require('./database');
 
-const findGameWithJoinCode = joinCode => db.Game.findOne({ joinCode });
+const findGameWithJoinCode = joinCode => Game.findOne({ joinCode });
+const addPlayerToGame = (joinCode, player) => findGameWithJoinCode().then((game) => {
+  if (!game) {
+    return Promise.reject(`There are no games with the join code: ${joinCode}`);
+  } else if (game.players.length < game.maxPlayers) {
+    return Promise.reject('The maximum amount of players has been reached.');
+  }
+  game.players.push(player);
+  return game.save();
+});
 
 exports.findGameWithJoinCode = findGameWithJoinCode;
+exports.addPlayerToGame = addPlayerToGame;
