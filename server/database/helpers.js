c052cd2d0a7175cc084fb305a7db4022347636f6
Cain <cainwatson@users.noreply.github.com>
2018-06-05 00:33:17 -0500

    (fix) add player to game helper function

--- server/database/helpers.js
+++ server/database/helpers.js
@@ -4,8 +4,8 @@ const findGameWithJoinCode = joinCode => Game.findOne({ joinCode })
   .then(game => game || Promise.reject(`There are no games with the join code: ${joinCode}`))
   .catch(() => Promise.reject(`Something went wrong when finding a game with the join code: ${joinCode}`));
 
-const addPlayerToGame = (joinCode, player) => findGameWithJoinCode().then((game) => {
-  if (game.players.length < game.maxPlayers) {
+const addPlayerToGame = (joinCode, player) => findGameWithJoinCode(joinCode).then((game) => {
+  if (game.players.length >= game.maxPlayers) {
     return Promise.reject('The maximum amount of players has been reached.');
   }
   game.players.push(player);
