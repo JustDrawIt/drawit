a0c77bbef17d4a65cdc33760d58a43f3932a1bf0
Cain <cainwatson@users.noreply.github.com>
2018-06-05 15:57:53 -0500

    (refactor) more listener/emitter namespacing

--- server/listeners/game/join.js
+++ server/listeners/game/join.js
@@ -9,7 +9,7 @@ module.exports = ({ data, socket, io }) => {
       socket.isAdmin = isAdmin;
       socket.hasDrawn = false;
 
-      socket.join(joinCode, () => io.in(joinCode).emit('joined', { nickname }));
+      socket.join(joinCode, () => io.in(joinCode).emit('round:joined', { nickname }));
     })
-    .catch(error => socket.emit('not_joined', { error }));
+    .catch(error => socket.emit('round:not_joined', { error }));
 };
