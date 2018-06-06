b97df5c9312071315ca9098d3afb022ded6b6b7c
Eric O'Neal Jr <ejeric23@gmail.com>
2018-06-06 09:31:01 -0500

    added a logout route

---
+++ server/routers/logout.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const passport = require('passport');
+const passportSetup = require('../passport');
+
+const logout = express.Router();
+
+logout.get('/', (req, res) => {
+  req.logout();
+  res.redirect('/login');
+});
