5b5db6c1fe5bb77d2790bed6d0cb5351cc8fb815
Eric O'Neal Jr <ejeric23@gmail.com>
2018-06-05 23:37:19 -0500

    serialized and deserialized users

--- server/passport.js
+++ server/passport.js
@@ -1,7 +1,17 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 const keys = require('./keys');
-const user = require('./database/database');
+const db = require('./database/database');
+// mongodb id
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  db.User.findByid(id).then((user) => {
+    done(null, user);
+  });
+});
 
 passport.use(new GoogleStrategy({
   // options for google strategy
@@ -11,20 +21,22 @@ passport.use(new GoogleStrategy({
 }, (accessToken, refreshToken, profile, done) => {
   // passport Callback function
   // check if user exists in database
-  user.UserfindOne({
+  db.User.findOne({
     googleId: profile.id,
   })
     .then((currentUser) => {
       if (currentUser) {
       // User exists
         console.log(`user is: ${currentUser}`);
+        done(null, currentUser);
       }
       // create user
-      new user.User({
+      new db.User({
         username: profile.displayName,
         googleId: profile.id,
       }).save().then((newUser) => {
         console.log(`new user created: ${newUser}`);
+        done(null, newUser);
       });
     });
 }));
