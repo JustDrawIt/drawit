5b9af54ab956938f162d70f6b2b6ac6802e2c3ea
Eric O'Neal Jr <ejeric23@gmail.com>
2018-06-06 10:35:32 -0500

    Taking the authentication off /codes

--- server/routers/codes.js
+++ server/routers/codes.js
@@ -3,16 +3,8 @@ const { findGameWithJoinCode } = require('../database/helpers');
 
 const codes = express.Router();
 
-const authCheck = (req, res, next) => {
-  if (!req.user) {
-    res.redirect('/login');
-  } else {
-    next();
-  }
-};
 
-
-codes.post('/', authCheck, (req, res) => {
+codes.post('/', (req, res) => {
   const { joinCode } = req.body;
   findGameWithJoinCode(joinCode)
     .then(game => res.send({ valid: !!game, error: null }))
