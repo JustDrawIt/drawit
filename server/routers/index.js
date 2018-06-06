97f37cf7339d25bbbe29e3f621529a384df4b11d
Eric O'Neal Jr <ejeric23@gmail.com>
2018-06-06 10:04:15 -0500

    Fixed auth route

--- server/routers/index.js
+++ server/routers/index.js
@@ -1,13 +1,11 @@
 const gamesRouter = require('./games');
 const codesRouter = require('./codes');
 const authRouter = require('./auth');
-const authCallBack = require('./authCallBack');
 const logoutRouter = require('./logout');
 
 module.exports = (app) => {
   app.use('/games', gamesRouter);
   app.use('/codes', codesRouter);
   app.use('/auth', authRouter);
-  app.use('/auth', authCallBack);
   app.use('/logout', logoutRouter);
 };
