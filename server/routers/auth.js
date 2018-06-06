fb9ff911c1f711886539e6efaa3135c82ff26420
Cain <cainwatson@users.noreply.github.com>
2018-06-06 16:01:00 -0500

    (cleanup) move authenticated to passport directory

--- server/routers/auth.js
+++ server/routers/auth.js
@@ -3,12 +3,14 @@ const passport = require('passport');
 
 const auth = express.Router();
 
+auth.get('/', (req, res) => res.send({ user: req.user || null }));
+
 auth.get('/google', passport.authenticate('google', {
   scope: ['profile'],
 }));
-
-auth.get('/google/redirect', passport.authenticate('google'), (req, res) => {
-  res.redirect('/games');
-});
+auth.get('/google/redirect', passport.authenticate('google', {
+  successRedirect: '/',
+  failureRedirect: '/login',
+}));
 
 module.exports = auth;
--- server/routers/games.js
+++ server/routers/games.js
@@ -1,18 +1,11 @@
 const express = require('express');
 const shortid = require('shortid');
+const authenticated = require('../passport/authenticated');
 const { createGame } = require('../database/helpers');
 
 const games = express.Router();
 
-const authCheck = (req, res, next) => {
-  if (!req.user) {
-    res.redirect('/login');
-  } else {
-    next();
-  }
-};
-
-games.post('/', authCheck, (req, res) => {
+games.post('/', authenticated, (req, res) => {
   const { timePerRound, maxPlayers, maxRounds } = req.body;
   const joinCode = shortid.generate();
 
