e4869a2341b1164c70c333760ee0ac72b54337b3
Cain <cainwatson@users.noreply.github.com>
2018-06-07 03:44:20 -0500

    (create) base tool class

---
+++ client/components/Game/Canvas/tools/BaseTool.js
@@ -0,0 +1,114 @@
+import {
+  DEFAULT_SIZE,
+  DEFAULT_FILL,
+  DEFAULT_STROKE_COLOR,
+  DEFAULT_FILL_COLOR,
+} from './defaults';
+
+class BaseTool {
+  constructor(context, options = {}) {
+    this.context = context;
+    this.imageData = null;
+    this.item = null;
+    this.options = {
+      ...options,
+      tool: options.tool,
+      size: options.size || DEFAULT_SIZE,
+      fill: options.fill || DEFAULT_FILL,
+      strokeColor: options.strokeColor || DEFAULT_STROKE_COLOR,
+      fillColor: options.fillColor || DEFAULT_FILL_COLOR,
+    };
+  }
+
+  setContext(context) {
+    this.context = context;
+  }
+  getContext() {
+    return this.context;
+  }
+
+  setOptions(options) {
+    this.options = {
+      ...this.options,
+      ...options,
+    };
+  }
+
+  create({ mouseX, mouseY }) {
+    const { options } = this;
+
+    return {
+      ...options,
+      start: {
+        x: mouseX,
+        y: mouseY,
+      },
+      end: {
+        x: null,
+        y: null,
+      },
+    };
+  }
+
+  drawShape(position) { }
+
+  draw(position) {
+    const { context, imageData, options } = this;
+
+    if (imageData) {
+      context.putImageData(imageData, 0, 0);
+    }
+
+    context.save();
+    context.beginPath();
+    context.lineJoin = 'round';
+    context.lineCap = 'round';
+    context.globalCompositeOperation = 'source-over';
+    context.lineWidth = options.size;
+    context.strokeStyle = options.strokeColor;
+    context.fillStyle = options.fill ? options.fillColor : 'transparent';
+
+    this.drawShape(position);
+
+    context.stroke();
+    context.fill();
+    context.closePath();
+    context.restore();
+  }
+
+  onMouseDown(position) {
+    const { context } = this;
+    const { clientWidth, clientHeight } = context.canvas;
+    const item = this.create(position);
+
+    this.item = item;
+    this.imageData = context.getImageData(0, 0, clientWidth, clientHeight);
+  }
+
+  onMouseMove(position) {
+    const { item } = this;
+
+    if (item) {
+      this.draw(position);
+    }
+  }
+
+  onMouseUp(position) {
+    const { item } = this;
+
+    if (item) {
+      item.end.x = position.mouseX;
+      item.end.y = position.mouseY;
+
+      this.onMouseMove(position);
+      this.imageData = null;
+      this.item = null;
+
+      return item;
+    }
+
+    return null;
+  }
+}
+
+export default BaseTool;
---
+++ client/components/Game/Canvas/tools/defaults.js
@@ -0,0 +1,7 @@
+import RectangleTool from './Rectangle/RectangleTool';
+
+export const DEFAULT_TOOL = RectangleTool;
+export const DEFAULT_SIZE = 2;
+export const DEFAULT_FILL = false;
+export const DEFAULT_STROKE_COLOR = 'black';
+export const DEFAULT_FILL_COLOR = 'transparent';
