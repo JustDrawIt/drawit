a50dc4f3e018db269ca171a64427beb0b934b0d2
Cain <cainwatson@users.noreply.github.com>
2018-06-07 16:56:27 -0500

    (change) move canvas tool defaults

--- client/components/Game/Canvas/defaults.js
+++ client/components/Game/Canvas/defaults.js
@@ -1,6 +1,6 @@
-import RectangleTool from './Rectangle/RectangleTool';
+import PencilTool from './tools/Pencil/PencilTool';
 
-export const DEFAULT_TOOL = RectangleTool;
+export const DEFAULT_TOOL = PencilTool;
 export const DEFAULT_SIZE = 2;
 export const DEFAULT_FILL = false;
 export const DEFAULT_STROKE_COLOR = 'black';
