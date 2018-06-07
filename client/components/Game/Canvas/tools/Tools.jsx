ecea0c69b260824ab9eb387b0160b34c55eceea8
Cain <cainwatson@users.noreply.github.com>
2018-06-07 03:47:10 -0500

    (create) tools component

---
+++ client/components/Game/Canvas/tools/Tools.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Pencil from './Pencil/Pencil';
+import Line from './Line/Line';
+import Rectangle from './Rectangle/Rectangle';
+import Ellipse from './Ellipse/Ellipse';
+
+const Tools = () => (
+  <div>
+    <Pencil />
+    <Line />
+    <Rectangle />
+    <Ellipse />
+  </div>
+);
+
+export default Tools;
