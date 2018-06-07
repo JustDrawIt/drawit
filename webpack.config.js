47feef202ba402f9d711024dac2981f535d08a92
Cain <cainwatson@users.noreply.github.com>
2018-06-07 01:12:27 -0500

    (add) public path to webpack config to fix absolute path issue

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,10 @@ module.exports = {
     ],
   },
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: ['.js', '.jsx'],
+  },
+  output: {
+    publicPath: '/',
   },
   plugins: [
     new HtmlWebPackPlugin({
