cefcdf43b8124878b1099052138899c2a8497915
Cain <cainwatson@users.noreply.github.com>
2018-06-07 17:05:22 -0500

    (change) adjust draw listener on the server to take the item from the payload

--- server/listeners/round/draw.js
+++ server/listeners/round/draw.js
@@ -1,4 +1,4 @@
 module.exports = ({ data, socket, io }) => {
-  const { mouseX, mouseY, joinCode } = data;
-  socket.to(joinCode).emit('round:drew', { mouseX, mouseY });
+  const { item, joinCode } = data;
+  socket.to(joinCode).emit('round:drew', { item });
 };
