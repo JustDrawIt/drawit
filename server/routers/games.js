f6280c9fb47c580b65a9c2846b1fdadd0f0f5cc6
Cain <cainwatson@users.noreply.github.com>
2018-06-08 16:20:24 -0500

    (add) return game on GET to /games with query param joinCode

--- server/routers/games.js
+++ server/routers/games.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const shortid = require('shortid');
 const authenticated = require('../passport/authenticated');
-const { createGame } = require('../database/helpers');
+const { findGameWithJoinCode, createGame } = require('../database/helpers');
 
 const games = express.Router();
+
+games.get('/', (req, res) => {
+  const { joinCode } = req.query;
+
+  findGameWithJoinCode(joinCode)
+    .then(game => res.send(game))
+    .catch(error => res.status(500).send({ error: error.message }));
+});
 
 games.post('/', authenticated, (req, res) => {
   const { maxPlayers, maxRounds } = req.body;
