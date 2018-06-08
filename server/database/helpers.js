dda13e0db85f8654b34311f7a1283a575afb7775
Cain <cainwatson@users.noreply.github.com>
2018-06-08 01:09:17 -0500

    (add) updateGameRound helper and use .exec

--- server/database/helpers.js
+++ server/database/helpers.js
@@ -1,20 +1,27 @@
 const { Game } = require('./database');
 
 const findGameWithJoinCode = joinCode => Game.findOne({ joinCode })
-  .then(game => game || Promise.reject(`There are no games with the join code: ${joinCode}`))
+  .exec()
+  .then(game => game || Promise.reject(new Error(`There are no games with the join code: ${joinCode}`)));
+
+const updateGameRound = (joinCode, word) => Game.findOneAndUpdate({ joinCode }, {
+  $set: { word },
+  $inc: { roundsPlayed: 1 },
+}).exec();
 
 const addPlayerToGame = (joinCode, nickname) => findGameWithJoinCode(joinCode).then((game) => {
   if (game.players.find(player => player.nickname === nickname)) {
-    return Promise.reject('Nickname is already used.');
+    return Promise.reject(new Error('Nickname is already used.'));
   } else if (game.players.length >= game.maxPlayers) {
-    return Promise.reject('The maximum amount of players has been reached.');
+    return Promise.reject(new Error('The maximum amount of players has been reached.'));
   }
   game.players.push({ nickname });
   return game.save();
 });
 
-const createGame = data => new Game(data).save().catch(() => Promise.reject('Something went wrong when creating a new game.'))
+const createGame = data => new Game(data).save().catch(() => Promise.reject(new Error('Something went wrong when creating a new game.')));
 
 exports.findGameWithJoinCode = findGameWithJoinCode;
+exports.updateGameRound = updateGameRound;
 exports.addPlayerToGame = addPlayerToGame;
 exports.createGame = createGame;
