6cdf253613a51b31e8131cecf3bdbd6013c880b9
Cain <cainwatson@users.noreply.github.com>
2018-06-09 17:32:15 -0500

    (change) notification styles

--- client/styles.js
+++ client/styles.js
@@ -7,6 +7,33 @@ export const injectGlobalStyles = () => injectGlobal`
   #app {
     position: relative;
   }
+
+  .notification {
+    height: fit-content !important;
+    border: 0 !important;
+    border-radius: 3px !important;
+    padding: 15px !important;
+    font-size: 16px !important;
+    font-family: 'Lato', sans-serif !important;
+  }
+  .notifications-tr {
+    right: 10px !important;
+    top: 10px !important;
+  }
+  .notification-error {
+    background-color: #FF7575E6 !important;
+    color: #f8f8f8 !important;
+  }
+  .notification-message {
+    margin-right: 20px !important;
+  }
+  .notification-dismiss {
+    background-color: transparent !important;
+    border-radius: 0 !important;
+    top: 12px !important;
+    right: 12px !important;
+    font-size: 23px !important;
+  }
 `;
 
 export const BorderStyles = css`
