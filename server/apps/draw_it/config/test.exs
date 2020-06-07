2d9d2577a2bce589cf63b2c672433600373b4e3c
Cain Watson <cainwatson00@gmail.com>
2020-06-07 16:28:32 -0500

    ci: add github action

--- server/apps/draw_it/config/test.exs
+++ server/apps/draw_it/config/test.exs
@@ -2,9 +2,9 @@ use Mix.Config
 
 # Configure database for test mode
 config :draw_it, DrawIt.Repo,
-  username: "postgres",
-  password: "postgres",
-  database: "draw_it_test",
-  hostname: "localhost",
+  username: System.get_env("POSTGRES_USERNAME", "postgres"),
+  password: System.get_env("POSTGRES_PASSWORD", "postgres"),
+  database: System.get_env("POSTGRES_DATABASE", "draw_it_test"),
+  hostname: System.get_env("POSTGRES_HOST", "localhost"),
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: 10
