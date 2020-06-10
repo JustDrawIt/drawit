fac8ecd3962e3d13366a5231f940cc0d819452e0
Cain Watson <cainwatson00@gmail.com>
2020-06-10 13:00:07 -0500

    feat: create round schema and migration

---
+++ server/apps/draw_it/lib/draw_it/games/round.ex
@@ -0,0 +1,22 @@
+defmodule DrawIt.Games.Round do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias DrawIt.Games.{Game, Player}
+
+  schema "game_rounds" do
+    field :word, :string
+
+    belongs_to :game, Game, foreign_key: :id_game
+    belongs_to :player_drawer, Player, foreign_key: :id_player_drawer
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(round, attrs) do
+    round
+    |> cast(attrs, [:id_game, :id_player_drawer, :word])
+    |> validate_required([:id_game, :id_player_drawer, :word])
+  end
+end
