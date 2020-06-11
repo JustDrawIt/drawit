88ac018bd65bd11d9011c4e3b1b1a3c237821b07
Cain Watson <cainwatson00@gmail.com>
2020-06-11 17:07:22 -0500

    feat: build games api

---
+++ server/apps/draw_it_web/lib/draw_it_web/controllers/game_controller.ex
@@ -0,0 +1,43 @@
+defmodule DrawItWeb.GameController do
+  use DrawItWeb, :controller
+
+  alias DrawIt.Games
+  alias DrawIt.Games.Game
+
+  action_fallback DrawItWeb.FallbackController
+
+  def index(conn, _params) do
+    games = Games.list_games()
+    render(conn, "index.json", games: games)
+  end
+
+  def create(conn, %{"game" => game_params}) do
+    with {:ok, %Game{} = game} <- Games.create_game(game_params) do
+      conn
+      |> put_status(:created)
+      |> put_resp_header("location", Routes.game_path(conn, :show, game))
+      |> render("show.json", game: game)
+    end
+  end
+
+  def show(conn, %{"id" => id}) do
+    game = Games.get_game!(id)
+    render(conn, "show.json", game: game)
+  end
+
+  def update(conn, %{"id" => id, "game" => game_params}) do
+    game = Games.get_game!(id)
+
+    with {:ok, %Game{} = game} <- Games.update_game(game, game_params) do
+      render(conn, "show.json", game: game)
+    end
+  end
+
+  def delete(conn, %{"id" => id}) do
+    game = Games.get_game!(id)
+
+    with {:ok, %Game{}} <- Games.delete_game(game) do
+      send_resp(conn, :no_content, "")
+    end
+  end
+end
