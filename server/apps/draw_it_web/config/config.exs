8885f5e8d1c36c21254a199390ee63f66abb8fa8
Cain Watson <cainwatson00@gmail.com>
2020-06-11 14:16:15 -0500

    chore: remove duplicate config

--- server/apps/draw_it_web/config/config.exs
+++ server/apps/draw_it_web/config/config.exs
@@ -17,11 +17,6 @@ config :draw_it_web, DrawItWeb.Endpoint,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
-# Configures Elixir's Logger
-config :logger, :console,
-  format: "$time $metadata[$level] $message\n",
-  metadata: [:request_id]
-
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
