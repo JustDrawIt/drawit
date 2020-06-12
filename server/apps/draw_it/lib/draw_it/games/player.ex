30f1897d4380d151c1d93d36a2282fb0b769d72a
Cain Watson <cainwatson00@gmail.com>
2020-06-11 20:35:27 -0500

    feat: add score to player schema

--- server/apps/draw_it/lib/draw_it/games/player.ex
+++ server/apps/draw_it/lib/draw_it/games/player.ex
@@ -6,6 +6,7 @@ defmodule DrawIt.Games.Player do
 
   schema "game_players" do
     field :nickname, :string
+    field :score, :integer, default: 0
 
     belongs_to :game, Game, foreign_key: :id_game
 
@@ -15,7 +16,7 @@ defmodule DrawIt.Games.Player do
   @doc false
   def changeset(player, attrs) do
     player
-    |> cast(attrs, [:id_game, :nickname])
+    |> cast(attrs, [:id_game, :nickname, :score])
     |> validate_required([:id_game, :nickname])
   end
 end
