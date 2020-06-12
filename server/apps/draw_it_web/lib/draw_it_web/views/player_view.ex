8d975a7451beefd96d728e465709d10e06cf464b
Cain Watson <cainwatson00@gmail.com>
2020-06-11 19:52:55 -0500

    feat: build game players api

---
+++ server/apps/draw_it_web/lib/draw_it_web/views/player_view.ex
@@ -0,0 +1,16 @@
+defmodule DrawItWeb.PlayerView do
+  use DrawItWeb, :view
+  alias DrawItWeb.PlayerView
+
+  def render("index.json", %{game_players: game_players}) do
+    %{data: render_many(game_players, PlayerView, "player.json")}
+  end
+
+  def render("show.json", %{player: player}) do
+    %{data: render_one(player, PlayerView, "player.json")}
+  end
+
+  def render("player.json", %{player: player}) do
+    %{id: player.id, nickname: player.nickname}
+  end
+end
