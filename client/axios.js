f68ac139efe4adb034f1d5ddedc5b94072567f53
Cain Watson <cainwatson00@gmail.com>
2020-06-13 17:11:38 -0500

    feat: update axios conf

--- client/axios.js
+++ client/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const PORT = process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : undefined;
-const instance = axios.create({ baseURL: PORT, withCredentials: true });
+const PORT = process.env.NODE_ENV === 'development' ? 'http://localhost:4000' : undefined;
+const instance = axios.create({ baseURL: PORT });
 
 export default instance;
