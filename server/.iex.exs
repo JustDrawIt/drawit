ecaf96f40dab519d879e9bc30f653d478699ce39
Cain Watson <cainwatson00@gmail.com>
2020-06-14 19:02:49 -0500

    chore: add .iex.exs

---
+++ server/.iex.exs
@@ -0,0 +1,7 @@
+alias DrawIt.Repo
+alias DrawIt.Games
+alias DrawIt.Games.{Game, Round, Player}
+
+alias DrawItWeb.{GameController, PlayerController, RoundController}
+alias DrawItWeb.{GameView, PlayerView, RoundView}
+alias DrawItWeb.{UserSocket, GameChannel}
