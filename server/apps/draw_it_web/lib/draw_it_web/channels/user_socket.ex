5cb61a1c009dceb7a8add78ba40b0bf1a51d0534
Cain Watson <cainwatson00@gmail.com>
2020-06-15 14:01:41 -0500

    feat: add game channel

--- server/apps/draw_it_web/lib/draw_it_web/channels/user_socket.ex
+++ server/apps/draw_it_web/lib/draw_it_web/channels/user_socket.ex
@@ -1,8 +1,10 @@
 defmodule DrawItWeb.UserSocket do
   use Phoenix.Socket
 
-  ## Channels
-  # channel "room:*", DrawItWeb.RoomChannel
+  ##
+  # Channels
+  ##
+  channel "game:*", DrawItWeb.GameChannel
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
