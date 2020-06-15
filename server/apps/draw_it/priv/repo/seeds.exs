517b9ae1e696812471c45ca4216d350d75c5a750
Cain Watson <cainwatson00@gmail.com>
2020-06-14 19:05:01 -0500

    chore: add seed data

--- server/apps/draw_it/priv/repo/seeds.exs
+++ server/apps/draw_it/priv/repo/seeds.exs
@@ -7,4 +7,51 @@
 #
 #     DrawIt.Factory.insert!(%DrawIt.SomeSchema{})
 
-alias DrawIt.Factory, warn: false
+# alias DrawIt.Factory, warn: false
+
+alias DrawIt.Games
+
+{:ok, game1} =
+  Games.create_game(%{
+    max_rounds: 3,
+    max_player: 3
+  })
+
+{:ok, game1_player1} =
+  Games.create_player(%{
+    id_game: game1.id,
+    nickname: "Octavia"
+  })
+
+{:ok, game1_player2} =
+  Games.create_player(%{
+    id_game: game1.id,
+    nickname: "Lauren"
+  })
+
+{:ok, game1_player3} =
+  Games.create_player(%{
+    id_game: game1.id,
+    nickname: "Cory"
+  })
+
+{:ok, _game1_round1} =
+  Games.create_round(%{
+    id_game: game1.id,
+    id_player_drawer: game1_player1.id,
+    word: "book"
+  })
+
+{:ok, _game1_round2} =
+  Games.create_round(%{
+    id_game: game1.id,
+    id_player_drawer: game1_player2.id,
+    word: "wall"
+  })
+
+_game1_round3 =
+  Games.create_round(%{
+    id_game: game1.id,
+    id_player_drawer: game1_player3.id,
+    word: "plant"
+  })
