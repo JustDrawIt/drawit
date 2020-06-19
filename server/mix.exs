7d4e78c65865c9ce4c746d79cdb5ea26d6ca2d73
Cain Watson <cainwatson00@gmail.com>
2020-06-19 08:42:18 -0500

    chore: install credo

--- server/mix.exs
+++ server/mix.exs
@@ -31,7 +31,8 @@ defmodule DrawIt.Umbrella.MixProject do
   defp deps do
     [
       {:ex_doc, ">= 0.0.0", only: [:dev, :test]},
-      {:mix_test_watch, "~> 1.0", only: :dev, runtime: false}
+      {:mix_test_watch, "~> 1.0", only: :dev, runtime: false},
+      {:credo, "~> 1.4", only: [:dev, :test], runtime: false}
     ]
   end
 end
