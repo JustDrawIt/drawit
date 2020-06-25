188bba8b40ccfde6f1da426033594ee61493c6fc
Cain Watson <cainwatson00@gmail.com>
2020-06-25 02:41:33 -0500

    chore: adjust game server logging

--- server/config/dev.exs
+++ server/config/dev.exs
@@ -1,4 +1,6 @@
 use Mix.Config
 
-# Do not include metadata nor timestamps in development logs
-config :logger, :console, format: "[$level] $message\n"
+# Do not include timestamps in development logs
+config :logger, :console,
+  format: "$metadata[$level] $message\n",
+  metadata: [:join_code, :player, :guess, :correct_word, :round_id]
