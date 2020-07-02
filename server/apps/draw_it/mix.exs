a4708baa95fa2c04f0a3f8f05b738f65c7e289cf
Cain Watson <cainwatson00@gmail.com>
2020-07-02 16:39:45 -0500

    chore: bump version

--- server/apps/draw_it/mix.exs
+++ server/apps/draw_it/mix.exs
@@ -4,7 +4,7 @@ defmodule DrawIt.MixProject do
   def project do
     [
       app: :draw_it,
-      version: "2.3.0",
+      version: "2.4.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
