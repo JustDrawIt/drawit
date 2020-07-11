125acdee34d5eb02f66651f9ff3eee9fb8aea5ae
Cain Watson <cainwatson00@gmail.com>
2020-07-11 18:44:43 -0500

    docs: add module doc to DrawIt.Id

--- server/apps/draw_it/lib/draw_it/id.ex
+++ server/apps/draw_it/lib/draw_it/id.ex
@@ -1,3 +1,12 @@
 defmodule DrawIt.Id do
+  @moduledoc """
+  This module exposes a `generate/0` method for creating unique ids.
+
+  ## Examples
+
+      iex> generate()
+      "YghmwYvDCTo3b1iMK1tZYb"
+  """
+
   use Puid, charset: :alphanum
 end
