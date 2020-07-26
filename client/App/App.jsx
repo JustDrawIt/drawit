65309dbff74dfb5dc1f674000252787135c7ca03
Cain Watson <cainwatson00@gmail.com>
2020-07-25 23:16:51 -0500

    test: add screen play test

--- client/App/App.jsx
+++ client/App/App.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
-
-import store from '../store';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import ScreenLogin from '../screens/ScreenLogin';
 import ScreenPlay from '../screens/ScreenPlay';
 import ScreenCreateGame from '../screens/ScreenCreateGame';
@@ -12,17 +9,13 @@ import { injectGlobalStyles } from '../styles';
 injectGlobalStyles();
 
 const App = () => (
-  <Provider store={store}>
-    <BrowserRouter>
-      <Switch>
-        <Route path="/login" component={ScreenLogin} />
-        <Route path="/play" component={ScreenPlay} />
-        <Route path="/create_game" component={ScreenCreateGame} />
-        <Route path="/games/:joinCode" component={ScreenGame} />
-        <Redirect to="/play" />
-      </Switch>
-    </BrowserRouter>
-  </Provider>
+  <Switch>
+    <Route path="/login" component={ScreenLogin} />
+    <Route path="/play" component={ScreenPlay} />
+    <Route path="/create_game" component={ScreenCreateGame} />
+    <Route path="/games/:joinCode" component={ScreenGame} />
+    <Redirect to="/play" />
+  </Switch>
 );
 
 export default App;
--- client/App/App.test.jsx
+++ client/App/App.test.jsx
@@ -1,10 +1,45 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createMemoryHistory } from 'history';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import App from './App';
+import store from '../store';
 
 describe('<App />', () => {
-  test('App Exists', () => {
-    const { baseElement } = render(<App />);
-    expect(baseElement).toBeDefined();
-  });
+  describe('<ScreenPlay', () => {
+    // TODO: Need to implement server mock. "Mock Service Worker" could work.
+    test.skip('displays join game form', async () => {
+      const history = createMemoryHistory();
+      render(
+        <Router history={history}>
+          <App />
+        </Router>
+      );
+
+      fireEvent.change(screen.getByPlaceholderText('Join code'), { target: { value: '_xf3ZtJ' } });
+      fireEvent.click(screen.getByText('Play!'));
+
+      await waitFor(() => screen.getByRole('heading'));
+      screen.debug();
+
+      expect(screen.getByRole('heading')).toHaveTextContent('Enter A Nickname');
+    });
+
+    test('displays create game button', () => {
+      const history = createMemoryHistory();
+      render(
+        <Provider store={store}>
+          <Router history={history}>
+            <App />
+          </Router>
+        </Provider>
+      );
+
+      fireEvent.click(screen.getByText('Create a game'));
+
+      expect(screen.getByRole('heading')).toHaveTextContent('Create Game');
+    });
+  })
 });
