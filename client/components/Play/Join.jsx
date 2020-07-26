9aa9a3c241d7c3eb9aa717fdf8b9faf4d1cf5d26
Cain Watson <cainwatson00@gmail.com>
2020-07-26 00:10:43 -0500

    refactor: use hooks in Join

--- client/components/Play/Join.jsx
+++ client/components/Play/Join.jsx
@@ -1,61 +1,47 @@
-import React, { PureComponent } from 'react';
+import React, { useState } from 'react';
 import ReactRouterPropTypes from 'react-router-prop-types';
 import axios from '../../axios';
 import Button from '../Utils/Button';
 import Input from '../Utils/Input';
 
-class JoinGame extends PureComponent {
-  constructor(props) {
-    super(props);
+const JoinGame = (props) => {
+  const { history } = props;
 
-    this.state = {
-      joinCode: '',
-      error: '',
-    };
+  const [joinCode, setJoinCode] = useState('');
+  const [error, setError] = useState('');
 
-    this.setJoinCode = this.setJoinCode.bind(this);
-    this.handleKeyPress = this.handleKeyPress.bind(this);
-    this.join = this.join.bind(this);
-  }
-
-  setJoinCode({ target }) {
-    this.setState({ joinCode: target.value });
-  }
-
-  handleKeyPress(event) {
+  const handleChangeJoinCode = event => setJoinCode(event.target.value);
+  const handleInputKeyPress = (event) => {
     if (event.key === 'Enter') {
-      this.join();
+      handleJoin();
     }
-  }
-
-  join() {
-    const { joinCode } = this.state;
-    const { history } = this.props;
-
-    if (joinCode.length >= 7) {
-      axios.get('/api/games', { params: { join_code: joinCode } })
-        .then(response => (response.data.data[0]
-          ? history.push(`/games/${joinCode}`)
-          : this.setState({ error: 'There are no games with that join code!' })
-        ))
-        .catch(() => this.setState({ error: 'There are no games with that join code!' }));
-    } else {
-      this.setState({ error: 'Join code must be at least 7 chars long.' });
+  };
+  const handleJoin = () => {
+    if (joinCode.length < 7) {
+      return setError('Join code must be at least 7 chars long.');
     }
-  }
 
-  render() {
-    const { error } = this.state;
+    axios.get('/api/games', { params: { join_code: joinCode } })
+      .then(response => (response.data.data[0]
+        ? history.push(`/games/${joinCode}`)
+        : setError('There are no games with that join code!')
+      ))
+      .catch(() => setError('There are no games with that join code!'));
+  };
 
-    return (
-      <div>
-        <Input onChange={this.setJoinCode} onKeyPress={this.handleKeyPress} placeholder="Join code" type="text" />
-        <Button onClick={this.join}>Play!</Button>
-        {error ? <p>{error}</p> : null}
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <Input
+        onChange={handleChangeJoinCode}
+        onKeyPress={handleInputKeyPress}
+        placeholder="Join code"
+        type="text"
+      />
+      <Button onClick={handleJoin}>Play!</Button>
+      {error ? <p>{error}</p> : null}
+    </div>
+  );
+};
 
 JoinGame.propTypes = {
   history: ReactRouterPropTypes.history.isRequired,
