1a7af40a0415134ddb3b254b63557e7f0924b275
Cain Watson <cainwatson00@gmail.com>
2020-07-27 09:30:40 -0500

    refactor: use hooks in Line

--- client/components/Game/Canvas/tools/Line/Line.jsx
+++ client/components/Game/Canvas/tools/Line/Line.jsx
@@ -1,43 +1,34 @@
-import React, { PureComponent } from 'react';
+import React, { useCallback } from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import { connect, useSelector } from 'react-redux';
 import { setToolAction } from '../../../../../store/actions/game.actions';
 import LineTool, { TOOL_LINE } from './LineTool';
 import ToolButton from '../../../../Utils/ToolButton';
 
-class Line extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.onClick = this.onClick.bind(this);
-  }
+const Line = (props) => {
+  const { dispatchTool } = props;
 
-  onClick() {
-    const { dispatchTool } = this.props;
-    const tool = new LineTool();
+  const isActive = useSelector(state => (
+    !!state.game.canvas.tool && (state.game.canvas.tool.name === TOOL_LINE)
+  ));
 
-    dispatchTool(tool);
-  }
+  const handleClick = useCallback(() => {
+    dispatchTool(new LineTool());
+  }, [dispatchTool]);
 
-  render() {
-    const { active } = this.props;
-
-    return (
-      <ToolButton onClick={this.onClick} active={active}>
-        <i className="fas fa-ruler" />
-      </ToolButton>
-    );
-  }
-}
+  return (
+    <ToolButton onClick={handleClick} active={isActive}>
+      <i className="fas fa-ruler" />
+    </ToolButton>
+  );
+};
 
 Line.propTypes = {
-  active: PropTypes.bool.isRequired,
   dispatchTool: PropTypes.func.isRequired,
 };
 
 export default connect(
-  ({ game }) => ({
-    active: !!game.canvas.tool && (game.canvas.tool.name === TOOL_LINE),
-  }),
+  null,
   dispatch => ({
     dispatchTool: setToolAction(dispatch),
   }),
